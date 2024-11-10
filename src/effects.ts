b7398d0bbbb594c47b3c9c72e28621ebb2f3318f
Peter Hörmeyer <peter@hoermeyer.net>
2024-11-10 18:34:14 +0100

    feat: improve positioning of dragged elements

--- src/effects.ts
+++ src/effects.ts
@@ -1,12 +1,15 @@
-export const updateElementPosition = (element: HTMLElement, mousePosition: {x: number, y: number}) => {
+export const updateElementPosition = (element: HTMLElement, mousePosition: { x: number, y: number }) => {
+    if (element.style.position !== 'fixed') {
+        element.style.setProperty('position', 'fixed');
+        element.style.setProperty('cursor', 'grabbing');
+        element.style.setProperty('z-index', '9999');
+    }
+
     window.requestAnimationFrame(() => {
-        if (mousePosition && element) {
-            element.style.setProperty('position', 'fixed');
-            element.style.setProperty('cursor', 'grabbing');
-            element.style.setProperty('z-index', '9999');
-            element.style.setProperty('top', `${mousePosition.y - 20}px`);
-            element.style.setProperty('left', `${mousePosition.x - 20}px`);
-        }
+        // get element top and left coords
+        const clientRect: DOMRect = element.getBoundingClientRect();
+        element.style.setProperty('top', `${mousePosition.y - clientRect.height / 2}px`);
+        element.style.setProperty('left', `${mousePosition.x - clientRect.width / 2}px`);
     });
 }
 
